9744c950f1aaf2afea63d48cd0f91c11ec0a56d0
fkrazy <f2kloa@gmail.com>
2017-07-19 16:06:41 -0600

    rangos

--- WindowsApplication2/Form1.Designer.vb
+++ WindowsApplication2/Form1.Designer.vb
@@ -28,20 +28,20 @@ Partial Class Form1
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.B_frecuencias = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.NOD_rango = New System.Windows.Forms.NumericUpDown()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.B_parentesis_final = New System.Windows.Forms.Button()
         Me.B_parentecis_inicial = New System.Windows.Forms.Button()
         Me.NUD_rango_inicio = New System.Windows.Forms.NumericUpDown()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.NUD_rango = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.B_frecuencias = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         Me.TabPage2.SuspendLayout()
-        CType(Me.NOD_rango, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NUD_rango_inicio, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
+        CType(Me.NUD_rango_inicio, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUD_rango, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -85,7 +85,7 @@ Partial Class Form1
         Me.TabPage1.Location = New System.Drawing.Point(4, 25)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(665, 39)
+        Me.TabPage1.Size = New System.Drawing.Size(665, 74)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "ordenar"
         Me.TabPage1.UseVisualStyleBackColor = True
@@ -93,7 +93,7 @@ Partial Class Form1
         'TabPage2
         '
         Me.TabPage2.Controls.Add(Me.GroupBox1)
-        Me.TabPage2.Controls.Add(Me.NOD_rango)
+        Me.TabPage2.Controls.Add(Me.NUD_rango)
         Me.TabPage2.Controls.Add(Me.Label1)
         Me.TabPage2.Controls.Add(Me.B_frecuencias)
         Me.TabPage2.Location = New System.Drawing.Point(4, 25)
@@ -104,37 +104,32 @@ Partial Class Form1
         Me.TabPage2.Text = "frecuencias"
         Me.TabPage2.UseVisualStyleBackColor = True
         '
-        'B_frecuencias
+        'GroupBox1
         '
-        Me.B_frecuencias.Location = New System.Drawing.Point(465, 10)
-        Me.B_frecuencias.Name = "B_frecuencias"
-        Me.B_frecuencias.Size = New System.Drawing.Size(75, 23)
-        Me.B_frecuencias.TabIndex = 0
-        Me.B_frecuencias.Text = "crear frecuencias"
-        Me.B_frecuencias.UseVisualStyleBackColor = True
+        Me.GroupBox1.Controls.Add(Me.B_parentesis_final)
+        Me.GroupBox1.Controls.Add(Me.B_parentecis_inicial)
+        Me.GroupBox1.Controls.Add(Me.NUD_rango_inicio)
+        Me.GroupBox1.Location = New System.Drawing.Point(180, 10)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(174, 65)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Numero de inicio"
         '
-        'Label1
+        'B_parentesis_final
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(29, 13)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(50, 17)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Rango"
-        '
-        'NOD_rango
-        '
-        Me.NOD_rango.Location = New System.Drawing.Point(26, 33)
-        Me.NOD_rango.Name = "NOD_rango"
-        Me.NOD_rango.Size = New System.Drawing.Size(53, 22)
-        Me.NOD_rango.TabIndex = 2
-        Me.NOD_rango.Value = New Decimal(New Integer() {3, 0, 0, 0})
+        Me.B_parentesis_final.Location = New System.Drawing.Point(119, 20)
+        Me.B_parentesis_final.Name = "B_parentesis_final"
+        Me.B_parentesis_final.Size = New System.Drawing.Size(22, 31)
+        Me.B_parentesis_final.TabIndex = 6
+        Me.B_parentesis_final.Text = ")"
+        Me.B_parentesis_final.UseVisualStyleBackColor = True
         '
         'B_parentecis_inicial
         '
-        Me.B_parentecis_inicial.Location = New System.Drawing.Point(30, 20)
+        Me.B_parentecis_inicial.Location = New System.Drawing.Point(24, 20)
         Me.B_parentecis_inicial.Name = "B_parentecis_inicial"
-        Me.B_parentecis_inicial.Size = New System.Drawing.Size(16, 31)
+        Me.B_parentecis_inicial.Size = New System.Drawing.Size(22, 31)
         Me.B_parentecis_inicial.TabIndex = 3
         Me.B_parentecis_inicial.Text = "("
         Me.B_parentecis_inicial.UseVisualStyleBackColor = True
@@ -146,26 +141,31 @@ Partial Class Form1
         Me.NUD_rango_inicio.Size = New System.Drawing.Size(48, 22)
         Me.NUD_rango_inicio.TabIndex = 4
         '
-        'Button1
+        'NUD_rango
         '
-        Me.Button1.Location = New System.Drawing.Point(119, 20)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(22, 31)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = ")"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.NUD_rango.Location = New System.Drawing.Point(26, 33)
+        Me.NUD_rango.Name = "NUD_rango"
+        Me.NUD_rango.Size = New System.Drawing.Size(53, 22)
+        Me.NUD_rango.TabIndex = 2
+        Me.NUD_rango.Value = New Decimal(New Integer() {3, 0, 0, 0})
         '
-        'GroupBox1
+        'Label1
         '
-        Me.GroupBox1.Controls.Add(Me.Button1)
-        Me.GroupBox1.Controls.Add(Me.B_parentecis_inicial)
-        Me.GroupBox1.Controls.Add(Me.NUD_rango_inicio)
-        Me.GroupBox1.Location = New System.Drawing.Point(180, 10)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(174, 65)
-        Me.GroupBox1.TabIndex = 7
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Numero de inicio"
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Rango"
+        '
+        'B_frecuencias
+        '
+        Me.B_frecuencias.Location = New System.Drawing.Point(465, 10)
+        Me.B_frecuencias.Name = "B_frecuencias"
+        Me.B_frecuencias.Size = New System.Drawing.Size(75, 23)
+        Me.B_frecuencias.TabIndex = 0
+        Me.B_frecuencias.Text = "crear frecuencias"
+        Me.B_frecuencias.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -182,9 +182,9 @@ Partial Class Form1
         Me.TabPage1.ResumeLayout(False)
         Me.TabPage2.ResumeLayout(False)
         Me.TabPage2.PerformLayout()
-        CType(Me.NOD_rango, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NUD_rango_inicio, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
+        CType(Me.NUD_rango_inicio, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUD_rango, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -196,11 +196,11 @@ Partial Class Form1
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
     Friend WithEvents TabPage2 As TabPage
-    Friend WithEvents NOD_rango As NumericUpDown
+    Friend WithEvents NUD_rango As NumericUpDown
     Friend WithEvents Label1 As Label
     Friend WithEvents B_frecuencias As Button
     Friend WithEvents B_parentecis_inicial As Button
-    Friend WithEvents Button1 As Button
+    Friend WithEvents B_parentesis_final As Button
     Friend WithEvents NUD_rango_inicio As NumericUpDown
     Friend WithEvents GroupBox1 As GroupBox
 End Class
--- WindowsApplication2/Form1.vb
+++ WindowsApplication2/Form1.vb
@@ -1,15 +1,15 @@
 ﻿Public Class Form1
-    Dim datos As DataTable
+    Dim datos As New DataTable
     Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
         If e.KeyCode = Keys.Enter Then
             Try
                 Dim dato As Integer
                 dato = Int(TextBox1.Text)
-                datos.NewRow()
-                datos.Rows(datos.Rows.Count - 1).Item(0) = dato
+                datos.Rows.Add(datos.NewRow())
+                datos.Rows(datos.Rows.Count - 1).Item(1) = dato
                 TextBox1.Text = ""
             Catch ex As Exception
-                L_resultado.Text = ex.ToString
+                MsgBox(ex.ToString, MsgBoxStyle.OkOnly)
             End Try
         End If
     End Sub
@@ -18,14 +18,36 @@
         Dim vista As DataView = New DataView(datos)
         vista.Sort = "datos"
         DataGridView1.DataSource = vista
+        NUD_rango_inicio.Value = vista.Item(0).Item(1) - 1
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim primaria(1) As DataColumn
+        datos.Columns.Add("id")
+        datos.Columns("id").Unique = True
+        datos.Columns("id").AutoIncrement = True
+        primaria(0) = datos.Columns("id")
+        datos.PrimaryKey = primaria
         datos.Columns.Add("datos")
+        datos.Columns("datos").DataType = System.Type.GetType("System.Int32")
     End Sub
 
     Private Sub B_frecuencias_Click(sender As Object, e As EventArgs) Handles B_frecuencias.Click
-        Dim tabla_frecuencias As DataTable
+        Dim tabla_frecuencias As New DataTable
+        tabla_frecuencias.Columns.Add("clase")
+        tabla_frecuencias.Columns.Add("ni")
+        Dim maximo As Integer = datos.Compute("MAX(datos)", "")
+        Dim actual As Integer = NUD_rango_inicio.Value
+        Dim rango As Integer = NUD_rango.Value
+        Dim fila As Integer = 0
+        While actual < maximo
+            tabla_frecuencias.Rows.Add(tabla_frecuencias.NewRow())
+            fila = tabla_frecuencias.Rows.Count - 1
+            tabla_frecuencias.Rows(fila).Item(0) = B_parentecis_inicial.Text & actual & "-" & actual + rango & B_parentesis_final.Text
+            tabla_frecuencias.Rows(fila).Item(1) = datos.Compute("Count(datos)", "datos >" & actual & "and datos <=" & actual + rango)
+            actual += rango
+        End While
+        DataGridView1.DataSource = tabla_frecuencias
     End Sub
 
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles B_parentecis_inicial.Click
@@ -36,11 +58,15 @@
         End If
     End Sub
 
-    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
-        If B_parentecis_inicial.Text = ")" Then
-            B_parentecis_inicial.Text = "]"
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles B_parentesis_final.Click
+        If B_parentesis_final.Text = ")" Then
+            B_parentesis_final.Text = "]"
         Else
-            B_parentecis_inicial.Text = ")"
+            B_parentesis_final.Text = ")"
         End If
+    End Sub
+
+    Private Sub TabPage2_Click(sender As Object, e As EventArgs) Handles TabPage2.Click
+
     End Sub
 End Class
